da24824ed09b01193bdf4e7d23f689a258f84eb5
Marty <martynolan17@gmail.com>
2022-06-17 15:16:37 -0400

    added questions/answers for quiz

---
+++ script.js
@@ -0,0 +1,42 @@
+var quizQuestions = [{
+
+        Question: "What method would you use to create a DOM object Element?", 
+        A: ".getAttribute()", 
+        B: ".createElement()", 
+        C: ".getElementById", 
+        D: ".setAttribute()", 
+        Answer: ".createElement()"
+    
+},
+{
+    Question: "What are variables used for?", 
+    A: "Iterating over arrays", 
+    B: "Linking a JavaScript file to your html", 
+    C: "Storing data", 
+    D: "performing specific tasks", 
+    Answer: "Storing data"
+},
+{
+    Question: "When declaring a function, what comes after the keyword 'fucntion'?", 
+    A: "()", 
+    B: ";", 
+    C: "/", 
+    D: "++", 
+    Answer: "()"
+}, 
+{
+    Question: "What would you use if you wanted to execut a block of code a number of times?", 
+    A: "While Loop", 
+    B: "Math.random()", 
+    C: "For loop", 
+    D: "Switch statement", 
+    Answer: "For loop"
+}, 
+{
+    Question: "Using the word 'break' will stop the execution inside the switch block.", 
+    A: "True", 
+    B: "False", 
+    Answer: "True"
+}
+]
+
